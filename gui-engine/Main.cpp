2650e3b41acf819b419de12c6d85fb7b204abf06
shlyapp <shlyapp.exe@gmail.com>
2021-09-11 22:05:49 +0400

    Add test code

--- gui-engine/Main.cpp
+++ gui-engine/Main.cpp
@@ -1,6 +1,22 @@
 ﻿#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
 }
