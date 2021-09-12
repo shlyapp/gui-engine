4bff28e0e37819723538fbcff2039e8719904aab
shlyapp <shlyapp.exe@gmail.com>
2021-09-12 11:19:09 +0400

    Test code for event handler

--- gui-engine/GUICore.h
+++ gui-engine/GUICore.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <list>
 
 namespace gui
 {
-	class Compoment;
+	class Component;
 
 	enum class EventType
 	{
@@ -21,13 +22,94 @@ namespace gui
 
 	};
 
-	class Component
+	class Component : public sf::Drawable
 	{
-	private:
+	public:
+
+		std::list<IEventListener*> listeners_;
+
+		mutable EventType event_;
+
+		sf::RenderWindow* window_;
 
 		sf::Vector2f position_;
 		sf::Vector2f size_;
 
+		Component(sf::Vector2f position, sf::Vector2f size, sf::RenderWindow* window) :
+			position_(position),
+			size_(size),
+			window_(window),
+			event_(EventType::MouseLeave)
+		{
+
+		}
+
+		void notifyListeners() const
+		{
+			for (auto listener : listeners_)
+			{
+				listener->updateByGUIEvent(event_, this);
+			}
+		}
+
+		virtual void click() const
+		{
+			if (event_ != EventType::Click)
+			{
+				event_ = EventType::Click;
+				notifyListeners();
+			}
+		}
+
+		virtual void enter() const
+		{
+			if (event_ != EventType::MouseEnter)
+			{
+				event_ = EventType::MouseEnter;
+				notifyListeners();
+			}
+		}
+
+		virtual void leave() const
+		{
+			if (event_ != EventType::MouseLeave)
+			{
+				event_ = EventType::MouseLeave;
+				notifyListeners();
+			}
+		}
+
+		void draw(sf::RenderTarget& target, sf::RenderStates animation_state) const override
+		{
+			sf::Vector2f mouse_pos = window_->mapPixelToCoords(sf::Mouse::getPosition(*window_));
+
+			if (sf::IntRect(position_.x, position_.y, size_.x, size_.y).contains(mouse_pos.x, mouse_pos.y))
+			{
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+				{
+					click();
+				}
+				else
+				{
+					enter();
+				}
+			}
+			else
+			{
+				leave();
+			}
+		}
+
+		void addListener(IEventListener* listener)
+		{
+			listeners_.push_back(listener);
+		}
+
+		void remoteListener(IEventListener* listener)
+		{
+			listeners_.remove(listener);
+		}
 	};
+
 }
 
--- gui-engine/Main.cpp
+++ gui-engine/Main.cpp
@@ -1,9 +1,35 @@
 ﻿#include <iostream>
-#include <SFML/Graphics.hpp>
+#include "GUICore.h"
+
+class Test : public gui::IEventListener
+{
+public:
+
+    void updateByGUIEvent(gui::EventType type, const gui::Component* component) override
+    {
+        switch (type)
+        {
+        case gui::EventType::Click:
+            std::cout << "click!\n";
+            break;
+        case gui::EventType::MouseEnter:
+            std::cout << "enter!\n";
+            break;
+        case gui::EventType::MouseLeave:
+            std::cout << "leave!\n";
+            break;
+        }
+    }
+};
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+
+    Test test;
+
+    gui::Component component(sf::Vector2f(0, 0), sf::Vector2f(100, 100), &window);
+    component.addListener(&test);
 
     while (window.isOpen())
     {
@@ -15,6 +41,7 @@ int main()
         }
 
         window.clear();
+        window.draw(component);
         window.display();
     }
 
