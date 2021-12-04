7121d8dd4e34865220acd66b20f618984cc4c904
Vailderp <59770659+Vailderp@users.noreply.github.com>
2021-12-05 00:21:37 +0700

    Update GUICore.h

--- gui-engine/GUICore.h
+++ gui-engine/GUICore.h
@@ -460,7 +460,7 @@ namespace gui
 
 		sf::Vector2 <sf::Color> colors_;
 		
-		satic inline constexpr sf::Vector2f m_sfml_bounds = { 2, 12 };
+		static inline constexpr sf::Vector2f m_sfml_bounds = { 2, 12 };
 
 		void InitText(const std::string text)
 		{
